1c914a9b8c6c5de3de8340b6e0fbc43265603242
Robin Garg <robingarg.mca@gmail.com>
2023-01-22 10:29:48 +0530

    Created 'FeedStoreSpec' that must be implemented by any 'FeedStore' test implementation to guarantee it meets specs

--- EssentialFeedTests/Feed Cache/CodeableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodeableFeedStoreTests.swift
@@ -8,41 +8,7 @@
 import XCTest
 import EssentialFeed
 
-protocol FeedStoreSpecs {
-    func test_load_deliversEmptyOnEmptyCache()
-    func test_load_hasNoSideEffectOnEmptyCache()
-    func test_load_deliversFoundValuesOnNonEmptyCache()
-    func test_load_hasNoSideEffectsOnNonEmptyCache()
-
-    func test_insert_deliversNoErrorOnEmptyCache()
-    func test_insert_deliversNoErrorOnNonEmptyCache()
-    func test_insert_overridesPreviouslyInsertedCachedValues()
-
-    func test_delete_deliversNoErrorOnEmptyCache()
-    func test_delete_hasNoSideEffectsOnEmptyCache()
-    func test_delete_deliversNoErrorOnNonEmptyCache()
-    func test_delete_emptiesPreviouslyInsertedCache()
-
-    func test_storeSideEffects_runSerially()
-
-}
-
-protocol FailableLoadFeedStoreSpecs {
-    func test_load_deliversFailureOnLoadError()
-    func test_load_hasNoSideEffectsOnFailure()
-}
-
-protocol FailableInsertFeedStoreSpecs {
-    func test_insert_deliversFailureOnInsertionError()
-    func test_insert_hasNoSideEffectOnInsertionError()
-}
-
-protocol FailableDeleteFeedStoreSpecs {
-    func test_delete_deliversErrorOnDeletionError()
-    func test_delete_hasNoSideEffectsOnDeletionError()
-}
-
-final class CodeableFeedStoreTests: XCTestCase {
+final class CodeableFeedStoreTests: XCTestCase, FailableFeedStore {
     override func setUp() {
         super.setUp()
         
