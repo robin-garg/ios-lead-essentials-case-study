2a25180523ec8ceec3475edfc39a51ee8ba2c087
Robin Garg <robingarg.mca@gmail.com>
2023-01-28 16:04:30 +0530

    `CoreDataFeedStore` loads empty on empty cache.

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -6,15 +6,27 @@
 //
 
 import XCTest
+import EssentialFeed
 
-private class CoreDataFeedStore {
+private class CoreDataFeedStore: FeedStore {
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
     
+    func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    func loadCachedFeed(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
 }
 
 final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     func test_load_deliversEmptyOnEmptyCache() {
         let sut = CoreDataFeedStore()
         
+        assertThatLoadDeliversEmptyOnEmptyCache(on: sut)
     }
     
     func test_load_hasNoSideEffectOnEmptyCache() {
