3e95c5ee546731c9045c0134cb7643a7db162e64
Robin Garg <robingarg.mca@gmail.com>
2023-01-28 19:58:49 +0530

    Add `ManagedCache` and `ManagedFeedImage` modal representation.

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -5,7 +5,7 @@
 //  Created by Apple on 28/01/23.
 //
 
-import Foundation
+import CoreData
 
 public final class CoreDataFeedStore: FeedStore {
     public init() {}
@@ -22,3 +22,20 @@ public final class CoreDataFeedStore: FeedStore {
         
     }
 }
+
+private class ManagedCache: NSManagedObject {
+    
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+}
+
+private class ManagedFeedImage: NSManagedObject {
+    
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
+}
+
+
