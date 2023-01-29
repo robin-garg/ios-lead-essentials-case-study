f4ed619697a71474745ab043bcd67768526dbb5b
Robin Garg <robingarg.mca@gmail.com>
2023-01-29 16:43:13 +0530

    Replace custom `LoadFeedResult` enum with the standard `Swift.Result`

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -58,7 +58,7 @@ extension LocalFeedLoader {
 }
 
 extension LocalFeedLoader: FeedLoader {
-    public typealias LoadResult = LoadFeedResult
+    public typealias LoadResult = FeedLoader.Result
 
     public func load(completion: @escaping (LoadResult) -> Void) {
         store.loadCachedFeed { [weak self] result in
