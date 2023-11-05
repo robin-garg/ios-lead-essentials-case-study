0a488cf595cdefc60cc2c84755017c746302a957
Robin Garg <robingarg.mca@gmail.com>
2023-11-05 09:50:48 +0530

    Move `UIImageView` animations to separate file

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -49,16 +49,3 @@ final class FeedImageCellController: FeedImageView {
         cell = nil
     }
 }
-
-extension UIImageView {
-    func setImageAnimated(_ newImage: UIImage?) {
-        image = newImage
-        
-        guard newImage != nil else { return }
-
-        self.alpha = 0
-        UIView.animate(withDuration: 0.25) {
-            self.alpha = 1
-        }
-    }
-}
