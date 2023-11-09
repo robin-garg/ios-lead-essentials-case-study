c116338cb492836b2430c2086f204790f9ab09b3
Robin Garg <robin.garg@netsmartz.net>
2023-11-09 06:33:54 +0530

    Move error view to its own file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Netsmartz on 09/11/23.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
