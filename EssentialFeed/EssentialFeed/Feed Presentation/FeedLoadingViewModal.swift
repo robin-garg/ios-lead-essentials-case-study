56ed008fa3ca55698d96d21377fa1d9d968ba364
Robin Garg <robin.garg@netsmartz.net>
2023-11-10 17:20:28 +0530

    Move `FeedLoadingViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModal.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModal.swift
+//  EssentialFeed
+//
+//  Created by Netsmartz on 10/11/23.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
