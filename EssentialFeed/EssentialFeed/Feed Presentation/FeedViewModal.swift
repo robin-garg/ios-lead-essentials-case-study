a48086735b09960a941fbca1c4b72809e463ca71
Robin Garg <robin.garg@netsmartz.net>
2023-11-10 17:19:33 +0530

    Move `FeedViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModal.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModal.swift
+//  EssentialFeed
+//
+//  Created by Netsmartz on 10/11/23.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
