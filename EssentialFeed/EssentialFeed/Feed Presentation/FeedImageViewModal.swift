499a532605f635be2f648d8433fb7fc535e03afc
Robin Garg <robin.garg@netsmartz.net>
2023-11-11 07:20:36 +0530

    Move `FeedImageViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModal.swift
@@ -0,0 +1,20 @@
+//
+//  FeedImageViewModal.swift
+//  EssentialFeed
+//
+//  Created by Netsmartz on 11/11/23.
+//
+
+import Foundation
+
+public struct FeedImageViewModel<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+    
+    public var hasLocation: Bool {
+        return location != nil
+    }
+}
