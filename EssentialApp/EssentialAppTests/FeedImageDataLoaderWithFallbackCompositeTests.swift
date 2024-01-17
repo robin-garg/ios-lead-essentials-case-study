df29d955f5ac5daa01a2c98f17c2da3b54516e9d
Robin Garg <robin.garg@netsmartz.net>
2024-01-17 20:07:56 +0530

    `FeedImageDataLoaderWithFallbackComposite.init` doesn't load image data

---
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,63 @@
+//
+//  FeedImageDataLoaderWithFallbackCompositeTests.swift
+//  EssentialAppTests
+//
+//  Created by Netsmartz on 16/01/24.
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
+    
+    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+
+    }
+    
+    private class TaskWrapper: FeedImageDataLoaderTask {
+        func cancel() {
+            
+        }
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return TaskWrapper()
+    }
+}
+
+class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
+    func test_init_doesNotLoadImageData() {
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        _ = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        
+        XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loaded urls in the primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded urls in the fallback loader")
+    }
+    
+    // MARK: - Helpers
+    
+    private class LoaderSpy: FeedImageDataLoader {
+        private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+        
+        var loadedURLs: [URL] {
+            return messages.map { $0.url }
+        }
+        
+        private class TaskWrapper: FeedImageDataLoaderTask {
+            func cancel() {
+                
+            }
+        }
+        
+        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+            messages.append((url, completion))
+            return TaskWrapper()
+        }
+    }
+        
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
+    }
+    
+}
